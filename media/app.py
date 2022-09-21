66c3d65e2bde7a1615d85b6eaf149bfd874d18e4
dmytromayor <mayor4090@gmail.com>
2022-09-21 13:15:25 +0300

    Add app

---
+++ media/app.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key, Controller
+import webbrowser
+
+keyboard = Controller()
+
+handlers = {}
+
+
+def add_handler(command):
+    def decorator(func):
+        handlers[command] = func
+        return func
+
+    return decorator
+
+
+@add_handler(command="start")
+def start_playing(param):
+    keyboard.tap(Key.media_play_pause)
+
+
+@add_handler(command="stop")
+def stop_playing(param):
+    keyboard.tap(Key.media_play_pause)
+
+
+@add_handler(command="next")
+def next_video(param):
+    keyboard.tap(Key.media_next)
+
+
+@add_handler(command="open_url")
+def open_url(param):
+    if isinstance(param, str):
+        webbrowser.open(param)
