aed372a340772a800941b8ab9cdbf7b91da3f91b
dmytromayor <mayor4090@gmail.com>
2022-10-08 16:09:14 +0300

    Config update for heroku

--- app/core/config.py
+++ app/core/config.py
@@ -1,26 +1,34 @@
 from starlette.config import Config
 
-DEBUG = False
+DEBUG = True
 
 config = None
 
 if DEBUG:
     config = Config(".env")
-else:
-    config = Config("/etc/secrets/.env")
 
-MYSQL_USER = config("MYSQL_USER", cast=str, default="")
-MYSQL_PASSWORD = config("MYSQL_PASSWORD", cast=str, default="")
-MYSQL_SERVER = config("MYSQL_SERVER", cast=str, default="")
-MYSQL_PORT = config("MYSQL_PORT", cast=str, default="")
-MYSQL_DB = config("MYSQL_DB", cast=str, default="")
+    MYSQL_USER = config("MYSQL_USER", cast=str, default="")
+    MYSQL_PASSWORD = config("MYSQL_PASSWORD", cast=str, default="")
+    MYSQL_SERVER = config("MYSQL_SERVER", cast=str, default="")
+    MYSQL_PORT = config("MYSQL_PORT", cast=str, default="")
+    MYSQL_DB = config("MYSQL_DB", cast=str, default="")
 
-if DEBUG:
     DATABASE_URL = "sqlite:///./sql_app.db"
-else:
-    DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
 
-BASE_DIR = config("BASE_DIR", cast=str, default="")
-ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=1)
-JWT_SECRET = config("JWT_SECRET", cast=str, default="")
-JWT_ALGORITHM = config("JWT_ALGORITHM", cast=str, default="")
+    BASE_DIR = config("BASE_DIR", cast=str, default="")
+    ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=1)
+    JWT_SECRET = config("JWT_SECRET", cast=str, default="")
+    JWT_ALGORITHM = config("JWT_ALGORITHM", cast=str, default="")
+else:
+    import os
+
+    MYSQL_USER = str(os.getenv("MYSQL_USER"))
+    MYSQL_PASSWORD = str(os.getenv("MYSQL_PASSWORD"))
+    MYSQL_SERVER = str(os.getenv("MYSQL_SERVER"))
+    MYSQL_PORT = str(os.getenv("MYSQL_PORT"))
+    MYSQL_DB = str(os.getenv("MYSQL_DB"))
+
+    BASE_DIR = str(os.getenv("BASE_DIR"))
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    JWT_SECRET = str(os.getenv("JWT_SECRET"))
+    JWT_ALGORITHM = str(os.getenv("JWT_ALGORITHM"))
