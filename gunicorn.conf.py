55316e5d31bc5b40689706eda500ea8e21daa143
dmytromayor <mayor4090@gmail.com>
2022-10-08 16:14:05 +0300

    Update config for gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,10 @@
 import multiprocessing
 
 
-bind = "localhost:80"
+bind = "127.0.0.1:8000"
 worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 120
+timeout = 60
 
 # workers = multiprocessing.cpu_count() * 2 + 1
-workers = 2
+workers = 1
+threads = multiprocessing.cpu_count() * 2 + 1
