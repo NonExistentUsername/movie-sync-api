d816f0df6503c03e7f70e75b80ea0f5e9f0908f2
dmytromayor <mayor4090@gmail.com>
2022-10-08 20:03:47 +0300

    Test fix

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: python runserver.py
+web: uvicorn main:app --app-dir app --workers 1 --host 0.0.0.0 --port $PORT
+worker: uvicorn main:app --app-dir app --workers 1 --host 0.0.0.0 --port 5000
 worker: cd bot && python bot.py
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+import os
 from fastapi import FastAPI
 from handlers import router
-
 from db.session import engine
 from db.base_class import Base
 from fastapi_pagination import add_pagination
@@ -23,3 +23,8 @@ app = get_application()
 # @app.on_event("startup")
 # def init():
 #     core.global_variables.init()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", app_dir="app", port=8000, host="127.0.0.1", reload=True, workers=1)
