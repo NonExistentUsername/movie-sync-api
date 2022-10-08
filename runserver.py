5a7eacea78080d2ea729b682e5cc4a0205d52b27
dmytromayor <mayor4090@gmail.com>
2022-10-08 19:41:25 +0300

    Try fix

--- runserver.py
+++ runserver.py
@@ -4,4 +4,4 @@ import uvicorn
 if __name__ == '__main__':
     port = int(os.getenv("PORT"))
     os.environ["BIND_PORT"] = str(port)
-    uvicorn.run("app.main:app", app_dir="app", port=port, host="0.0.0.0", reload=True, workers=1)
+    uvicorn.run("app.main:app", app_dir="app/app", port=port, host="0.0.0.0", reload=True, workers=1)
