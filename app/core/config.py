4f2dd80af39f7ac5687c05661be76ec5d5612cc8
dmytromayor <mayor4090@gmail.com>
2022-10-09 12:08:55 +0300

    Update config

--- app/core/config.py
+++ app/core/config.py
@@ -7,12 +7,6 @@ config = None
 if DEBUG:
     config = Config(".env")
 
-    MYSQL_USER = config("MYSQL_USER", cast=str, default="")
-    MYSQL_PASSWORD = config("MYSQL_PASSWORD", cast=str, default="")
-    MYSQL_SERVER = config("MYSQL_SERVER", cast=str, default="")
-    MYSQL_PORT = config("MYSQL_PORT", cast=str, default="")
-    MYSQL_DB = config("MYSQL_DB", cast=str, default="")
-
     DATABASE_URL = "sqlite:///./sql_app.db"
 
     BASE_DIR = config("BASE_DIR", cast=str, default="")
@@ -22,13 +16,14 @@ if DEBUG:
 else:
     import os
 
-    MYSQL_USER = str(os.getenv("MYSQL_USER"))
-    MYSQL_PASSWORD = str(os.getenv("MYSQL_PASSWORD"))
-    MYSQL_SERVER = str(os.getenv("MYSQL_SERVER"))
-    MYSQL_PORT = str(os.getenv("MYSQL_PORT"))
-    MYSQL_DB = str(os.getenv("MYSQL_DB"))
-
-    DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
+    # MYSQL_USER = str(os.getenv("MYSQL_USER"))
+    # MYSQL_PASSWORD = str(os.getenv("MYSQL_PASSWORD"))
+    # MYSQL_SERVER = str(os.getenv("MYSQL_SERVER"))
+    # MYSQL_PORT = str(os.getenv("MYSQL_PORT"))
+    # MYSQL_DB = str(os.getenv("MYSQL_DB"))
+    #
+    # DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
+    DATABASE_URL = str(os.getenv('DATABASE_URL'))
     
     BASE_DIR = str(os.getenv("BASE_DIR"))
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
