f124a86806162f63bd58dcafba669b4e31731f8c
dmytromayor <mayor4090@gmail.com>
2022-10-09 16:17:00 +0300

    Add websocket listening url

---
+++ app/handlers/app_ws.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
+from sqlalchemy.orm import Session
+import schemas.user
+import schemas.command
+import schemas.app
+import models.user
+import crud.user
+import crud.command
+import crud.app
+import core.deps
+import core.global_variables
+from starlette.responses import FileResponse
+
+
+app_ws_router = APIRouter()
+
+
+@app_ws_router.websocket("/get_commands")
+async def ws_get_updates(websocket: WebSocket, current_user: models.user.User = Depends(core.deps.get_current_user_from_websocket)):
+    await crud.command.ws_get_commands(websocket, current_user)
