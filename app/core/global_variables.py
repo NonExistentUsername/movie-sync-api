8e3080e18af48188485d2c270af2731db90a4492
dmytromayor <mayor4090@gmail.com>
2022-10-09 13:16:04 +0300

    Add socket manager

---
+++ app/core/global_variables.py
@@ -0,0 +1,27 @@
+from fastapi import WebSocket
+from typing import List
+
+
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data):
+        for connection in self.active_connections:
+            await connection.send_json(data)
+
+
+socket_manager: SocketManager = None
+
+
+def init():
+    global socket_manager
+    socket_manager = SocketManager()
+
