f4ee54ee58462470ba0c46bb0852c886c586dc4c
dmytromayor <mayor4090@gmail.com>
2022-11-12 17:24:37 +0200

    Variables will now be loaded only from the environment

--- app/core/config.py
+++ app/core/config.py
@@ -1,25 +1,9 @@
-from starlette.config import Config
 import os
 
 DEBUG = (os.getenv("DEBUG") == "True")
 
-if DEBUG:
-    config = Config(".env")
-
-    DATABASE_URL = "sqlite:///./sql_app.db"
-
-    BASE_DIR = config("BASE_DIR", cast=str, default="")
-    ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=1)
-    JWT_SECRET = config("JWT_SECRET", cast=str, default="")
-    JWT_ALGORITHM = config("JWT_ALGORITHM", cast=str, default="")
-else:
-    import os
-
-    DATABASE_URL = str(os.getenv('DATABASE_URL'))
-    # Replace 'postgres' to 'postgresql'
-    DATABASE_URL = "postgresql" + DATABASE_URL[8:]
-    
-    BASE_DIR = str(os.getenv("BASE_DIR"))
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-    JWT_SECRET = str(os.getenv("JWT_SECRET"))
-    JWT_ALGORITHM = str(os.getenv("JWT_ALGORITHM"))
+BASE_DIR = str(os.getenv("BASE_DIR"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+JWT_SECRET = str(os.getenv("JWT_SECRET"))
+JWT_ALGORITHM = str(os.getenv("JWT_ALGORITHM"))
+DATABASE_URL = str(os.getenv('DATABASE_URL'))
