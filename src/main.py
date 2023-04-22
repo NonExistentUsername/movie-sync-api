d6ccde57b5209364c51d3280977857539b11ec7d
dmytromayor <mayor4090@gmail.com>
2023-04-22 19:52:29 +0300

    Add alembic

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from fastapi_pagination import add_pagination
 import core.global_variables
 from db.base_class import Base
 from db.session import engine
-from db.utils import init_db
 from handlers.all_handlers import api_router
-from models.associations import *
-from models.room import *
+from models.associations import user_room_member_association_table
+from models.room import Room
+from models.user import User
 
 # Base.metadata.create_all(bind=engine)
 
@@ -54,8 +54,3 @@ app = get_application()
 @app.on_event("startup")
 def init():
     core.global_variables.init()
-
-
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
