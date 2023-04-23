b84822ee410840b892f6fd4582b75ee346152f8b
dmytromayor <mayor4090@gmail.com>
2023-04-23 16:17:24 +0300

    Refactoring

--- src/models/room.py
+++ src/models/room.py
@@ -2,10 +2,11 @@ import datetime
 import random
 import string
 
-from db.base_class import Base
-from models.associations import user_room_member_association_table
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+from db.base_class import Base
+from models.associations import user_room_member_association_table
 
 
 def gen_key(key_len: int = 8) -> str:
@@ -16,8 +17,10 @@ class Room(Base):
     __tablename__ = "rooms"
 
     id = Column(Integer, primary_key=True)
+
     name = Column(String(32), nullable=False, unique=True)
     key = Column(String(8), nullable=False, default=gen_key)
+
     creator_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
     members_of_room = relationship(
         "User",
