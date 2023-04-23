6b6ae5e8e18b9793286a69df78f3206e7eedac50
dmytromayor <mayor4090@gmail.com>
2023-04-23 18:09:14 +0300

    Add list of rooms to User domain model

--- src/domain/user.py
+++ src/domain/user.py
@@ -5,15 +5,20 @@ from domain.permission import PermissionsGroup
 
 
 class User:
-    def __init__(self, id: int, username: str, hashes_password: str, permissions: PermissionsGroup):
+    def __init__(self, id: int, username: str, hashes_password: str, permissions: PermissionsGroup, rooms: t.Set[int]):
         self._id = id
         self.username = username
         self.hashes_password = hashes_password
         self.permissions = permissions
+        self._rooms = rooms
 
     @property
     def id(self) -> int:
         return self._id
+
+    @property
+    def rooms(self) -> t.Set[int]:
+        return self._rooms
 
     def __eq__(self, __value: object) -> bool:
         if isinstance(__value, User):
