49d2b9ce46f91b3f1d6062ae3d0bff2432473651
dmytromayor <mayor4090@gmail.com>
2023-04-23 16:51:41 +0300

    Update security.py

--- src/core/security.py
+++ src/core/security.py
@@ -1,11 +1,1 @@
-from passlib.context import CryptContext
-
-PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto", truncate_error=True)
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return PWD_CONTEXT.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str) -> str:
-    return PWD_CONTEXT.hash(password)
+from core.auth import get_password_hash, verify_password
