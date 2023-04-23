724d959dd14039c0bf4bc372e39ec51e461e4b1a
dmytromayor <mayor4090@gmail.com>
2023-04-23 18:09:03 +0300

    Add list of members to Room domain model

--- src/domain/room.py
+++ src/domain/room.py
@@ -2,16 +2,21 @@ import typing as t
 
 
 class Room:
-    def __init__(self, id: int, name: str, key: str, creator_id: int, capacity: int) -> None:
+    def __init__(self, id: int, name: str, key: str, creator_id: int, capacity: int, members: t.Set[int]) -> None:
         self._id = id
         self.name = name
         self.key = key
         self.creator_id = creator_id
         self.capacity = capacity
+        self._members = members
 
     @property
     def id(self) -> int:
         return self._id
+
+    @property
+    def members(self) -> t.Set[int]:
+        return self._members
 
     def join(self, user_id: int) -> bool:
         raise NotImplementedError()
