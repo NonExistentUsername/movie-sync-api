514dbf31249c73d87e307f92c041f2e8f1601fde
dmytromayor <mayor4090@gmail.com>
2023-04-23 18:14:19 +0300

    Add ability to join/leave from room

--- src/domain/room.py
+++ src/domain/room.py
@@ -7,7 +7,7 @@ class Room:
         self.name = name
         self.key = key
         self.creator_id = creator_id
-        self.capacity = capacity
+        self._capacity = capacity
         self._members = members
 
     @property
@@ -18,8 +18,24 @@ class Room:
     def members(self) -> t.Set[int]:
         return self._members
 
-    def join(self, user_id: int) -> bool:
-        raise NotImplementedError()
+    @property
+    def capacity(self) -> int:
+        return self._capacity
+
+    def join(self, user_id: int) -> None:
+        if user_id in self._members:
+            raise ValueError("The user is already in the room")
+
+        if len(self._members) == self.capacity:
+            raise ValueError("The room is full")
+
+        self._members.add(user_id)
+
+    def leave(self, user_id: int) -> None:
+        if user_id not in self._members:
+            raise ValueError("User not in the room")
+
+        self._members.remove(user_id)
 
     def __eq__(self, __value: object) -> bool:
         if isinstance(__value, Room):
