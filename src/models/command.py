112104bc59bf9892c9e67758ecb9f7f7e8861b27
dmytromayor <mayor4090@gmail.com>
2023-04-23 16:16:43 +0300

    Refactoring

--- src/models/command.py
+++ src/models/command.py
@@ -1,16 +1,20 @@
 import datetime
 
-from db.base_class import Base
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+
+from db.base_class import Base
 
 
 class Command(Base):
     __tablename__ = "commands"
 
     id = Column(Integer, primary_key=True)
+
     command = Column(String(64), nullable=False)
     param = Column(String(256), nullable=True)
+
     receiver_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
     sender_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     room_id = Column(Integer, ForeignKey("rooms.id"), nullable=False)
+
     create_date = Column(DateTime, default=datetime.datetime.utcnow)
